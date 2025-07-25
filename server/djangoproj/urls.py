2602b7bf89127079a21f9af74e22e51264bbf730
Rohal Kabir <kabirrohal7@gmail.com>
2025-07-25 02:26:13 -0400

    changes project

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -18,9 +18,17 @@ from django.urls import path, include
 from django.views.generic import TemplateView
 from django.conf.urls.static import static
 from django.conf import settings
+from djangoapp.views import logout_request
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('about/', TemplateView.as_view(template_name="About.html")),
+    path('contact/', TemplateView.as_view(template_name="Contact.html")),
+    path('login/', TemplateView.as_view(template_name="index.html")),
+    path("djangoapp/logout", logout_request),
+    path('register/', TemplateView.as_view(template_name="index.html")),
+    path('dealers/', TemplateView.as_view(template_name='index.html')),
     path('djangoapp/', include('djangoapp.urls')),
     path('', TemplateView.as_view(template_name="Home.html")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
